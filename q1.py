29f5995837d90d94476fa0a78e4eb90ad1b753be
Hari041705 <mudadlahareesh75@gmail.com>
2026-02-16 21:04:16 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -4,4 +4,3 @@ def calculate_total_bill(amount: float, tip_percent: int) -> float:
     tip_percent = float(tip_percent)
     total = amount + (amount * tip_percent / 100)
     return round(total, 2)
-print(calculate_total_bill(100, 15))  
